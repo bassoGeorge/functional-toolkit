f4d055524db52209bc7b002f920f18618a7a8975
Anish ‘basso’ George <anishgeorgehb@gmail.com>
2020-03-06 15:06:36 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {DESCRIPTION, TITLE} from './sample-module'
+
+console.log(TITLE);
+console.log(DESCRIPTION);
+console.log("||============================================================================||")
